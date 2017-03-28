bbaa393cebef2d95787b6eb2f98e380a0c1b909f
davidangel <david@davidangel.net>
2017-03-28 15:48:39 -0400

    More template cleanup.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Forum
+                    {{ config('app.name') }}
                 </div>
             </div>
         </div>
