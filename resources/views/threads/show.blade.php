d6fe0be3b33209f60a843c5dd8bc7bf9e2f148eb
davidangel <david@davidangel.net>
2017-03-29 15:04:10 -0400

    Complete video 5.

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -20,5 +20,21 @@
             @endforeach
         </div>
     </div>
+
+    @if(auth()->check())
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <textarea name="body" id="body" class="form-control" placeholder="{{ auth()->user()->name }} replies..." rows="5"></textarea>
+                </div>
+                <button type="submit" class="btn btn-default">Post</button>
+            </form>
+        </div>
+    </div>
+    @else
+        <p class="text-center">Please <a href="{{ route('register') }}">register</a> or <a href="{{ route('login') }}">login</a> to participate in the conversation.</p>
+    @endif
 </div>
 @endsection
