1e40ceda39c7e90802771692b857cbc9ca0e5d6c
davidangel <david@davidangel.net>
2017-03-29 11:57:41 -0400

    Completed Video 3

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -5,11 +5,26 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">{{ $thread->title }}</div>
+                <div class="panel-heading"><strong>{{ $thread->title }}</strong></div>
                 <div class="panel-body">
                     {{ $thread->body }}
                 </div>
             </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            @foreach($thread->replies as $reply)
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <a href="">{{ $reply->owner->name }}</a> said {{ $reply->created_at->diffForHumans() }}...
+                </div>
+                <div class="panel-body">
+                    {{ $reply->body }}
+                </div>
+            </div>
+            @endforeach
         </div>
     </div>
 </div>
