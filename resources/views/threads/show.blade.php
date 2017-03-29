c2d1c239799e280ac93afc22e706ed27cb4c2822
davidangel <david@davidangel.net>
2017-03-29 14:31:18 -0400

    Complete Video 4

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading"><strong>{{ $thread->title }}</strong></div>
+                <div class="panel-heading"><strong><a href="">{{ $thread->creator->name }}</a> posted: {{ $thread->title }}</strong></div>
                 <div class="panel-body">
                     {{ $thread->body }}
                 </div>
@@ -16,14 +16,7 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             @foreach($thread->replies as $reply)
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <a href="">{{ $reply->owner->name }}</a> said {{ $reply->created_at->diffForHumans() }}...
-                </div>
-                <div class="panel-body">
-                    {{ $reply->body }}
-                </div>
-            </div>
+            @include('threads.reply')
             @endforeach
         </div>
     </div>
---
+++ tests/Feature/ParticipateInForumTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Auth\AuthenticationException;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ParticipateInForumTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function unauthenticated_users_may_not_add_replies()
+    {
+        $this->expectException(AuthenticationException::class);
+        $this->post('/threads/1/replies', []);
+    }
+
+    /** @test */
+    public function an_authenticated_user_may_participate_in_forum_threads()
+    {
+        $this->be(factory('App\User')->create());
+        $thread = factory('App\Thread')->create();
+        $reply = factory('App\Reply')->make([ 'thread_id' => $thread->id ]);
+
+        $this->post($thread->path() . '/replies', $reply->toArray());
+
+        $this->get($thread->path())->assertSee($reply->body);
+    }
+
+}
