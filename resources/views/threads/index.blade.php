5dd407306e99601f4c1f436060a0227c36873d8b
davidangel <david@davidangel.net>
2017-03-30 16:42:20 -0400

    Complete Video 11

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Forum Threads</div>
+                    <div class="panel-heading">Forum Threads <a href="/threads/create" class="btn btn-primary btn-xs pull-right">Add a Thread</a></div>
                     <div class="panel-body">
                         @foreach($threads as $thread)
                             <article>
